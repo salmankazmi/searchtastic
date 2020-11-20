bbb11d235d0b8898fb7ba28fbc5b326acfd2c771
slmnkzm@gmail.com <slmnkzm@gmail.com>
2020-11-20 16:54:25 +0530

    first commmit

---
+++ src/main/java/com/acme/searchtastic/SearchtasticApplication.java
@@ -0,0 +1,13 @@
+package com.acme.searchtastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchtasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchtasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/searchtastic/SearchtasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.searchtastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchtasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
